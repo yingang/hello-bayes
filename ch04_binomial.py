07414f81ab9c5f8d23f8909235ebafe5df143d85
Gang Yin <1246410+yingang@users.noreply.github.com>
2023-10-24 19:52:34 +0800

    chapter 5

--- ch04_binomial.py
+++ ch04_binomial.py
@@ -1,11 +1,11 @@
 from scipy.stats import binom
 
-print(f"Exercise #2: {binom.pmf(1, 5, p = 1/13) :.2%}")
+print(f"Exercise 4.2: {binom.pmf(1, 5, p = 1/13) :.2%}")
 
-print(f"Exercise #2: {5 * ((1/13)**1) * ((12/13)**4) :.2%}") # same with last one
+print(f"Exercise 4.2: {5 * ((1/13)**1) * ((12/13)**4) :.2%}") # same with last one
 
-print(f"Exercise #3: {binom.pmf(5, 10, p = 1/13) :.2%}")
+print(f"Exercise 4.3: {binom.pmf(5, 10, p = 1/13) :.2%}")
 
-print(f"Exercise #4: {binom.pmf(2, 7, p = 1/5) :.2%}")
+print(f"Exercise 4.4: {binom.pmf(2, 7, p = 1/5) :.2%}")
 
-print(f"Exercise #5: {binom.pmf(2, 25, p = 1/10) :.2%}")
+print(f"Exercise 4.5: {binom.pmf(2, 25, p = 1/10) :.2%}")
--- ch05_beta.py
+++ ch05_beta.py
@@ -1,13 +1,33 @@
 from scipy.stats import beta
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.integrate as intergrate
+import scipy.integrate as integrate
 
 a = 4
 b = 6
 
 x = np.arange(0.0, 1, 0.05)
-plt.plot(x, beta.pdf(x, a, b))
-plt.show()
+plt.plot(x, beta.pdf(x, a, b), label="5.1")
 
-print(intergrate.quad(lambda x: beta.pdf(x, a, b), 0.6, 1))
+y, err = integrate.quad(lambda x: beta.pdf(x, a, b), 0.6, 1)
+
+print(f"Exercise 5.1: {y:.2%}")
+
+a = 9
+b = 11
+plt.plot(x, beta.pdf(x, a, b), label="5.2")
+
+y, err = integrate.quad(lambda x: beta.pdf(x, a, b), 0.45, 0.55)
+
+print(f"Exercise 5.2: {y:.2%}")
+
+a = 109
+b = 111
+plt.plot(x, beta.pdf(x, a, b), label="5.3")
+
+y, err = integrate.quad(lambda x: beta.pdf(x, a, b), 0.45, 0.55)
+
+print(f"Exercise 5.3: {y:.2%}")
+
+plt.legend()
+plt.show()
