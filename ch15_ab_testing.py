782d5bc202fb7e42e9443507633e65cbf7194e29
Gang Yin <1246410+yingang@users.noreply.github.com>
2023-11-14 22:08:29 +0800

    chapter 15

--- ch15_ab_testing.py
+++ ch15_ab_testing.py
@@ -4,23 +4,66 @@ import numpy as np
 
 # Chapter 15.2
 
-alpha = 3
-beta = 7
-times = 100000
+prio_alpha = 3
+prio_beta = 7
+trials = 100000
 
-samples_a = np.random.beta(36 + alpha, 114 + beta, times)
-samples_b = np.random.beta(50 + alpha, 100 + beta, times)
+a_samples = np.random.beta(36 + prio_alpha, 114 + prio_beta, trials)
+b_samples = np.random.beta(50 + prio_alpha, 100 + prio_beta, trials)
 
-print(f"{np.sum(samples_a < samples_b) / times:.2%}")
+print(f"{np.sum(a_samples < b_samples) / trials:.2%}")
 
 plt.subplot(1, 2, 1)
-plt.hist(samples_b / samples_a)
+plt.hist(b_samples / a_samples)
 plt.xlabel('samples_b / samples_a')
 plt.ylabel('frequency')
 
 ax = plt.subplot(1, 2, 2)
-stats.ecdf(samples_b / samples_a).cdf.plot(ax)
+stats.ecdf(b_samples / a_samples).cdf.plot(ax)
 plt.xlabel('samples_b / samples_a')
 plt.ylabel('accumulated probability')
 
 plt.show()
+
+# Exercise 15.1
+
+prio_alpha = 300
+prio_beta = 700
+
+a_samples = np.random.beta(36 + prio_alpha, 114 + prio_beta, trials)
+b_samples = np.random.beta(50 + prio_alpha, 100 + prio_beta, trials)
+
+print(f"Exercise 15.1: {np.sum(a_samples < b_samples) / trials:.2%}")
+
+# Exercise 15.2
+
+a_alpha, a_beta = 30, 70
+b_alpha, b_beta = 20, 80
+
+a_samples = np.random.beta(36 + a_alpha, 114 + a_beta, trials)
+b_samples = np.random.beta(50 + b_alpha, 100 + b_beta, trials)
+
+print(f"Exercise 15.2: {np.sum(a_samples < b_samples) / trials:.2%}")
+
+# Exercise 15.3
+
+prio_alpha = 300    # director
+prio_beta = 700     # director
+
+a_rate = 0.25
+b_rate = 0.3
+
+b_win = 0
+emails = 0
+while b_win < 0.95:
+    emails += 100
+    a_positive = np.sum(np.random.uniform(size = emails//2) <= a_rate)  # A/B组对半分
+    a_negative = emails//2 - a_positive
+    b_positive = np.sum(np.random.uniform(size = emails//2) <= b_rate)
+    b_negative = emails//2 - b_positive
+
+    a_samples = np.random.beta(a_positive + prio_alpha, a_negative + prio_beta, trials)
+    b_samples = np.random.beta(b_positive + prio_alpha, b_negative + prio_beta, trials)
+    b_win = np.sum(a_samples < b_samples) / trials
+
+print(f"{emails} more emails to convince the director => {b_win:.2%}")
